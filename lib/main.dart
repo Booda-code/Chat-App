92f7d242fed8a90863d6a68bacee2701dee79ac7
Booda-code <abdoomda18@gmail.com>
2025-07-01 23:45:38 +0300

    Add : simple_bloc_observer.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:chat_app_fixed/screens/chat_screen.dart';
 import 'package:chat_app_fixed/screens/sign_in_screen.dart';
 import 'package:chat_app_fixed/screens/sign_up_screen.dart';
+import 'package:chat_app_fixed/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ChatApp());
